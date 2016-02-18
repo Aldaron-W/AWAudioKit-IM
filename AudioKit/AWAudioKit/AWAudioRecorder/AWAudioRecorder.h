d1519855fb1789a0752fddc04b298834c4cbec0f
Aldaron-W <AldaronWang@gmail.com>
2016-02-18 17:59:10 +0800

    Add recall delegate & error information

--- AudioKit/AWAudioKit/AWAudioRecorder/AWAudioRecorder.h
+++ AudioKit/AWAudioKit/AWAudioRecorder/AWAudioRecorder.h
@@ -9,15 +9,52 @@
 #import <Foundation/Foundation.h>
 #import <AudioToolbox/AudioToolbox.h>
 
+/**
+ *  错误标识
+ */
+typedef NS_ENUM(NSUInteger, AWAudioRecorderErrorCode) {
+    //====================      File        ====================
+    /**
+     *  关于文件操作的错误
+     */
+    AWAudioRecorderErrorCodeAboutFile = 0,
+    
+    //====================      Queue       ====================
+    /**
+     *  关于音频输入队列的错误
+     */
+    AWLAudioRecorderErrorCodeAboutQueue = 10,
+    /**
+     *  关于audio session的错误
+     */
+    AWAudioRecorderErrorCodeAboutSession = 20,
+    //====================      Control     ====================
+    /**
+     *  重复调用StartRecording函数
+     */
+    AWLAudioRecorderErrorCodeDoublePerformStartRecordingMethod = 30,
+    /**
+     *  重复调用StopRecording函数
+     */
+    AWLAudioRecorderErrorCodeDoublePerformStopRecordingMethod,
+    //====================      Others      ====================
+    /**
+     *  其他
+     */
+    AWAudioRecorderErrorCodeAboutOther,
+};
+
 @class AWAudioRecorder;
 
 @protocol AWAudioRecorderDelegate <NSObject>
 
 @optional
-//- (void)AudioRecorderDidStartRecording:(AWAudioRecorder *)audioRecorder;
-- (void)AudioRecorderDidStoppedRecording:(AWAudioRecorder *)audioRecorder;
+- (void)awAudioRecorderDidStartRecording:(AWAudioRecorder *)audioRecorder;
+- (void)awAudioRecorderDidStoppedRecording:(AWAudioRecorder *)audioRecorder;
+- (void)awAudioRecorderRecordingError:(AWAudioRecorder *)audioRecorder error:(NSError *)error;
 
-- (void)AudioRecorder:(AWAudioRecorder *)audioRecorder currentVolume:(float)volume;
+- (void)awAudioRecorder:(AWAudioRecorder *)audioRecorder currentVolume:(float)volume;
+
 
 @end
 
@@ -28,21 +65,49 @@
  *  是否正在录音
  */
 @property (atomic, assign, readonly) BOOL isRecording;
+/**
+ *  录音的采样率。默认：8000
+ */
+@property (atomic, assign, readonly) NSUInteger sampleRate;
+/**
+ *  缓冲区采集的秒数。默认：0.5秒
+ */
+@property (atomic, assign, readonly) double bufferDurationSeconds;
 
 #pragma mark - Public Property
-
 /**
- *  这俩是当前的采样率和缓冲区采集秒数，根据情况可以设置(对其设置必须在startRecording之前才有效)，随意设置可能有意外发生。
- *  这俩属性被标识为原子性的，读取写入是线程安全的。
+ *  录音的相关回调
  */
-@property (atomic, assign) NSUInteger sampleRate;
-@property (atomic, assign) double bufferDurationSeconds;
-
 @property (nonatomic, weak) id<AWAudioRecorderDelegate> delegate;
 
 #pragma mark - Public methods
 
+/**
+ *  在录音之前做的准备工作，此函数会获取采样率和缓冲区的采集秒数，并通过这两个参数初始化录音环境。
+ *  !注:本函数仅在录音未开始或录音结束后才可以进行调用。若录音时调用的话，这两个参数均不会生效，也不会重新初始化录音环境。
+ *      不过这两个参数均有默认值，而且在调用 startRecording 函数的时候会自动调取初始化录音环境的函数。
+ *      所以如果没有特殊需求的话，可以不调用此函数，直接调用 startRecording 函数即可开始录音。
+ *
+ *  @param sampleRate            采样率
+ *  @param bufferDurationSeconds 缓冲区采集秒数
+ *  @param error                 初始化异常信息
+ */
+- (void)prepareRecordingWithSampleRate:(NSUInteger)sampleRate andBufferDurationSeconds:(double)bufferDurationSeconds error:(NSError **)error;
+
+/**
+ *  开始录音
+ *  开始录音后会初始化录音环境，之后通过回调函数 AudioRecorderDidStartRecording:(AWAudioRecorder *)audioRecorder 通知delegate开始录音
+ *  若当前正处在录音的状态的话，则无法初始化录音环境，也不会调用任何回调函数
+ *  
+ *  @see AWAudioRecorderDelegate
+ *  @see - (void)AudioRecorderDidStartRecording:(AWAudioRecorder *)audioRecorder
+ */
 - (void)startRecording;
+
+/**
+ *  结束录音
+ *  若当前不处在录音状态的话，无法停止录音，也不会调用任何回调函数
+ */
 - (void)stopRecording;
 
 @end
--- AudioKit/AWAudioKit/AWAudioRecorder/AWAudioRecorder.m
+++ AudioKit/AWAudioKit/AWAudioRecorder/AWAudioRecorder.m
@@ -7,6 +7,23 @@
 //
 
 #import "AWAudioRecorder.h"
+#import <AVFoundation/AVFoundation.h>
+
+/**
+ *  缓存区的个数，3个一般不用改
+ */
+#define kAW_NumberAudioQueueBuffers 3
+
+/**
+ *  每次的音频输入队列缓存区所保存的是多少秒的数据
+ */
+#define kAW_DefaultBufferDurationSeconds 0.5
+/**
+ *  采样率，要转码为amr的话必须为8000
+ */
+#define kAW_DefaultSampleRate 8000
+
+#define kAWAudioRecorderErrorDomain @"AWAudioRecorderErrorDomain"
 
 @interface AWAudioRecorder (){
     //音频输入队列
@@ -19,9 +36,169 @@
  *  是否正在录音
  */
 @property (atomic, assign) BOOL isRecording;
+/**
+ *  录音的采样率。默认：8000
+ */
+@property (atomic, assign, getter=getSampleRate) NSUInteger sampleRate;
+/**
+ *  缓冲区采集的秒数。默认：0.5秒
+ */
+@property (atomic, assign, getter=getBufferDurationSeconds) double bufferDurationSeconds;
+
+@property (nonatomic, strong) NSLock *propertyLock;
 
 @end
 
 @implementation AWAudioRecorder
+
+#pragma mark - Object life cycle
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        //建立写入文件线程队列,串行，和一个信号量标识
+//        self.writeFileQueue = dispatch_queue_create("cn.aldaron.AWAudioRecorder.writeFileQueue", NULL);
+        
+        self.sampleRate = kAW_DefaultSampleRate;
+        self.bufferDurationSeconds = kAW_DefaultBufferDurationSeconds;
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sessionInterruption:)
+                                                     name:AVAudioSessionInterruptionNotification object:[AVAudioSession sharedInstance]];
+    }
+    return self;
+}
+
+#pragma mark - Public methods
+- (void)startRecording{
+    
+}
+
+- (void)stopRecording{
+    if (self.isRecording) {
+        self.isRecording = NO;
+        
+        //Recall
+        if (self.delegate && [self.delegate respondsToSelector:@selector(awAudioRecorderDidStoppedRecording:)]) {
+            [self.delegate awAudioRecorderDidStoppedRecording:self];
+        }
+    }
+    else{
+        [self recallErrorInfo:[self getErrorWithErrorCode:AWLAudioRecorderErrorCodeDoublePerformStopRecordingMethod]];
+    }
+}
+
+- (void)prepareRecordingWithSampleRate:(NSUInteger)sampleRate andBufferDurationSeconds:(double)bufferDurationSeconds error:(NSError **)error{
+    NSParameterAssert(sampleRate > 0);
+    NSParameterAssert(bufferDurationSeconds > 0);
+    
+    [self setSampleRate:sampleRate];
+    [self setBufferDurationSeconds:bufferDurationSeconds];
+}
+
+#pragma mark - Private methods
+#pragma mark Prepare recording environment
+/**
+ *  初始化录音环境
+ */
+- (void)prepareRecordingEnvironment{
+    
+}
+
+- (void)prepareAudioSession{
+    NSError *error = nil;
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
+}
+
+#pragma mark - Tools
+#pragma mark Recall Error
+- (void)recallErrorInfoAndStopRecording:(NSError *)error{
+    [self recallErrorInfo:error];
+    
+    if (self.isRecording) {
+        AudioQueueStop(_audioQueue, true);
+        AudioQueueDispose(_audioQueue, true);
+        [[AVAudioSession sharedInstance] setActive:NO error:nil];
+        
+        [self stopRecording];
+    }
+}
+
+- (void)recallErrorInfo:(NSError *)error{
+    if (self.delegate && [self.delegate respondsToSelector:@selector(awAudioRecorderRecordingError:error:)]) {
+        [self.delegate awAudioRecorderRecordingError:self error:error];
+    }
+}
+
+#pragma mark Error
+- (NSError *)getErrorWithErrorCode:(AWAudioRecorderErrorCode)errorCode{
+    NSError *error = nil;
+    NSString *description = nil;
+    
+    switch (errorCode) {
+        case AWAudioRecorderErrorCodeAboutFile:
+            description = @"关于文件操作的错误";
+            break;
+        case AWLAudioRecorderErrorCodeAboutQueue:
+            description = @"关于音频输入队列的错误";
+            break;
+        case AWAudioRecorderErrorCodeAboutSession:
+            description = @"关于audio session的错误";
+            break;
+        case AWLAudioRecorderErrorCodeDoublePerformStartRecordingMethod:
+            description = @"重复调用StartRecording函数";
+            break;
+        case AWLAudioRecorderErrorCodeDoublePerformStopRecordingMethod:
+            description = @"重复调用StopRecording函数";
+            break;
+        case AWAudioRecorderErrorCodeAboutOther:
+        default:
+            description = @"其他错误";
+            break;
+    }
+    
+    error = [NSError errorWithDomain:kAWAudioRecorderErrorDomain code:errorCode userInfo:@{NSLocalizedDescriptionKey:description}];
+    
+    return error;
+}
+
+#pragma mark - notification
+- (void)sessionInterruption:(NSNotification *)notification {
+    AVAudioSessionInterruptionType interruptionType = [[[notification userInfo]
+                                                        objectForKey:AVAudioSessionInterruptionTypeKey] unsignedIntegerValue];
+    if (AVAudioSessionInterruptionTypeBegan == interruptionType)
+    {
+        //DLOG(@"begin interruption");
+        //直接停止录音
+        [self stopRecording];
+    }
+    else if (AVAudioSessionInterruptionTypeEnded == interruptionType)
+    {
+        //DLOG(@"end interruption");
+    }
+}
+
+#pragma mark - Getters
+- (NSLock *)propertyLock{
+    if (_propertyLock) {
+        _propertyLock = [[NSLock alloc] init];
+    }
+    return _propertyLock;
+}
+
+- (NSUInteger)getSampleRate{
+    [self.propertyLock lock];
+    if (_sampleRate <= 0) {
+        return kAW_DefaultSampleRate;
+    }
+    [self.propertyLock unlock];
+    return _sampleRate;
+}
+
+- (double)getBufferDurationSeconds{
+    if (_bufferDurationSeconds <= 0.0f) {
+        return kAW_DefaultBufferDurationSeconds;
+    }
+    return _bufferDurationSeconds;
+}
 
 @end
