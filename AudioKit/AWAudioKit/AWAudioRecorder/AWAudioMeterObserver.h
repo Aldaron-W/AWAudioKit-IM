b416ae0f457e526437fe0f37a61c32ef96c0acb1
Aldaron-W <AldaronWang@gmail.com>
2016-02-18 11:38:27 +0800

    Create AWAudioMeterObserver files

---
+++ AudioKit/AWAudioKit/AWAudioRecorder/AWAudioMeterObserver.h
@@ -0,0 +1,13 @@
+//
+//  AWAudioMeterObserver.h
+//  Pods
+//
+//  Created by AldaronWang on 16/2/18.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AWAudioMeterObserver : NSObject
+
+@end
---
+++ AudioKit/AWAudioKit/AWAudioRecorder/AWAudioMeterObserver.m
@@ -0,0 +1,13 @@
+//
+//  AWAudioMeterObserver.m
+//  Pods
+//
+//  Created by AldaronWang on 16/2/18.
+//
+//
+
+#import "AWAudioMeterObserver.h"
+
+@implementation AWAudioMeterObserver
+
+@end
