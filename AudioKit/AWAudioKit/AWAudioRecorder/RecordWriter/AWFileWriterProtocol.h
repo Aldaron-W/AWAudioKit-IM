95d9694472f12e884ebf6195a060bbc984870998
Aldaron-W <AldaronWang@gmail.com>
2016-03-04 17:30:00 +0800

    实现编码文件协议

---
+++ AudioKit/AWAudioKit/AWAudioRecorder/RecordWriter/AWFileWriterProtocol.h
@@ -0,0 +1,47 @@
+//
+//  AWFileWriterProtocol.h
+//  Pods
+//
+//  Created by AldaronWang on 16/3/4.
+//
+//
+
+#import <AudioToolbox/AudioToolbox.h>
+
+@class AWAudioRecorder;
+
+#ifndef AWFileWriterProtocol_h
+#define AWFileWriterProtocol_h
+
+/**
+ *  处理写文件操作的，实际是转码的操作在其中进行。算作可扩展自定义的转码器
+ *  当然如果是实时语音的需求的话，就可以在此处理编码后发送语音数据到对方
+ *  PS:这里的三个方法是在后台线程中处理的
+ */
+@protocol AWFileWriterForMLAudioRecorder <NSObject>
+
+@optional
+- (AudioStreamBasicDescription)customAudioFormatBeforeCreateFile;
+
+@required
+/**
+ *  在录音开始时候建立文件和写入文件头信息等操作
+ *
+ */
+- (BOOL)createFileWithRecorder:(AWAudioRecorder*)recoder;
+
+/**
+ *  写入音频输入数据，内部处理转码等其他逻辑
+ *  能传递过来的都传递了。以方便多能扩展使用
+ */
+- (BOOL)writeIntoFileWithData:(NSData*)data withRecorder:(AWAudioRecorder*)recoder inAQ:(AudioQueueRef) inAQ inStartTime:(const AudioTimeStamp *)inStartTime inNumPackets:(UInt32)inNumPackets inPacketDesc:(const AudioStreamPacketDescription*)inPacketDesc;
+
+/**
+ *  文件写入完成之后的操作，例如文件句柄关闭等,isError表示是否是因为错误才调用的
+ *
+ */
+- (BOOL)completeWriteWithRecorder:(AWAudioRecorder*)recoder withIsError:(BOOL)isError;
+
+@end
+
+#endif /* AWFileWriterProtocol_h */
