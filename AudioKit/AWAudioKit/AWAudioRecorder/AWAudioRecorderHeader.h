ce603c84f35cf6917dc81a5e30fab8f54eee1844
Aldaron-W <AldaronWang@gmail.com>
2016-03-06 19:21:48 +0800

    添加 AWAudioKit 总头文件

---
+++ AudioKit/AWAudioKit/AWAudioRecorder/AWAudioRecorderHeader.h
@@ -0,0 +1,24 @@
+//
+//  AWAudioRecorderHeader.h
+//  Pods
+//
+//  Created by Work on 16/3/6.
+//
+//
+
+#ifndef AWAudioRecorderHeader_h
+#define AWAudioRecorderHeader_h
+
+//录音
+#import "AWAudioRecorder.h"
+
+//观察录音参数
+#import "AWAudioMeterObserver.h"
+
+//转码
+#import "AWFileWriterProtocol.h"
+//--MP3
+#import "MP3RecordWriter.h"
+
+
+#endif /* AWAudioRecorderHeader_h */
